b093cfb2cd9ea24f31ae3a4565fa71e3346f08ee
Berret Terry <berretterry@gmail.com>
2020-12-23 18:58:03 -0600

    page rendering

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import Preview from './preview.jsx'
 
-const App = () => (
-  <div>This text is in client/components/App.jsx</div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loginPopUp: false,
+      singUpPopUp: false,
+      imgurPopUp: false,
+      previewPopUp: false
+    };
+  }
+
+
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>Hi, Welcome!</h1>
+        <div>This will soon be gifLauncher!</div>
+      </div>
+    );
+  }
+};
 
 export default App;
 
